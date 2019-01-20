e5847991e94683f8b49399bc4bc4523ef8e136dc
EMEINT\Ahmed.Yousif <jousiffegy@gmail.com>
2019-01-20 17:39:32 +0200

    commit_01

--- food-recipe/src/app/app.module.ts
+++ food-recipe/src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent} from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent} from './recipes/recipe-list/recipe-list.component'
+import { RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component'
 import { ShoppingListComponent } from  './shopping-list/shopping-list.component'
 import { ShoppingEditComponent } from  './shopping-list/shopping-edit/shopping-edit.component'
@@ -17,6 +18,7 @@ import { ShoppingEditComponent } from  './shopping-list/shopping-edit/shopping-e
     HeaderComponent,
     RecipesComponent,
     RecipeListComponent,
+    RecipeItemComponent,
     RecipeDetailComponent,
     ShoppingListComponent,
     ShoppingEditComponent
