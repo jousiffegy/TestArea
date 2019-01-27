b65d94e5778e90a944abc96c4b24c0502245e575
EMEINT\Ahmed.Yousif <jousiffegy@gmail.com>
2019-01-27 12:22:09 +0200

    add router Link

--- food-recipe/src/app/app-routing.module.ts
+++ food-recipe/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {RecipesComponent} from '../app/recipes/recipes.component';
+import { ShoppingListComponent } from '../app/shopping-list/shopping-list.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'recipe',component: RecipesComponent },
+  {path:'shopping-list' , component:ShoppingListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
