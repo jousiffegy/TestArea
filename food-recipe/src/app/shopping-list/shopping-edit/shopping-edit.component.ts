dd13ae73483e8af9e6b2ace0d9b8e1ec0f1c5b47
Ahmed youssif <jousiffegy@gmail.com>
2019-02-13 21:20:12 +0200

    add input form

--- food-recipe/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ food-recipe/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,38 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+// import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+// import { Ingredient } from '../../shared/ingredient.model';
+// import { ShoppingListService } from '../shopping-list.service';
+
+// @Component({
+//   selector: 'app-shopping-edit',
+//   templateUrl: './shopping-edit.component.html',
+//   styleUrls: ['./shopping-edit.component.css']
+// })
+// export class ShoppingEditComponent implements OnInit {
+
+//   @ViewChild('nameInput') nameInputRef:ElementRef;
+//   @ViewChild('amountInput') amountInputRef:ElementRef;
+
+
+//   constructor(private shoppingListService:ShoppingListService) { }
+
+//   ngOnInit() {
+//   }
+//   onAddItem(){
+//     const name=this.nameInputRef.nativeElement.value;
+//     const amount=this.amountInputRef.nativeElement.value;
+
+//      const newIngrediant=new Ingredient(name,amount);
+//      this.shoppingListService.addIngredient(newIngrediant);
+
+//   }
+
+// }
+
+
+import { Component, OnInit} from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,19 +41,16 @@ import { ShoppingListService } from '../shopping-list.service';
 })
 export class ShoppingEditComponent implements OnInit {
 
-  @ViewChild('nameInput') nameInputRef:ElementRef;
-  @ViewChild('amountInput') amountInputRef:ElementRef;
+ 
 
 
   constructor(private shoppingListService:ShoppingListService) { }
 
   ngOnInit() {
   }
-  onAddItem(){
-    const name=this.nameInputRef.nativeElement.value;
-    const amount=this.amountInputRef.nativeElement.value;
-
-     const newIngrediant=new Ingredient(name,amount);
+  onAddItem(form:NgForm){
+    const value=form.value;
+     const newIngrediant=new Ingredient(value.name,value.amount);
      this.shoppingListService.addIngredient(newIngrediant);
 
   }
